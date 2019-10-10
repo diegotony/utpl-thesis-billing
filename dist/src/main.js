d81a73e65f1a43b0533c21923a41dd725d03aecb
diegotony <tucotony1396@gmail.com>
2019-10-10 16:34:15 -0500

    logging

--- dist/src/main.js
+++ dist/src/main.js
@@ -4,7 +4,7 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, { logger: ['error', 'warn'] });
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { logger: console });
     app.useGlobalPipes(new common_1.ValidationPipe());
     await app.listen(3000);
 }
