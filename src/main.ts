59cdc93e10796e99b2e51c6e4298920559c6208b
diegotony <tucotony1396@gmail.com>
2019-10-10 17:47:15 -0500

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,30 @@ import { ValidationPipe } from '@nestjs/common';
 import * as helmet from 'helmet';
 import * as csurf from 'csurf';
 import * as rateLimit from 'express-rate-limit';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(helmet());
   app.enableCors();
-  app.use(csurf());
+  // app.use(csurf());
   app.use(
     rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
       max: 100, // limit each IP to 100 requests per windowMs
     }),
   );
+
+  const options = new DocumentBuilder()
+  .setTitle('Billing api-rest')
+  .setDescription('The billing API description')
+  .setVersion('1.0')
+  .addTag('billing')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
