032a24e8d75081742132cbeb3bbe26807436f498
diegotony <tucotony1396@gmail.com>
2019-10-10 15:26:56 -0500

    travis config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BillingService } from './billing/billing.service';
+import { BillingController } from './billing/billing.controller';
+import { BillingModule } from './billing/billing.module';
+import { MongooseModule} from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BillingModule, MongooseModule.forRoot('mongodb://localhost/nest')],
+  controllers: [AppController, BillingController],
+  providers: [AppService, BillingService],
 })
 export class AppModule {}
