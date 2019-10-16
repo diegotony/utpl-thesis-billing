94dd856c158de9135c946a3639e8d4353b830e0f
diegotony <tucotony1396@gmail.com>
2019-10-16 00:38:10 -0500

    updated endpoint for api

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   .build();
 
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/billing', app, document);
 
   await app.listen(config.PORT);
 }
